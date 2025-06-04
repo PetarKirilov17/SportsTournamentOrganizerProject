919f546f8fc4619339867a9db3cdd2457ee5b30a
ldak <milen.com1@gmail.com>
2025-06-04 12:00:15 +0300

    server running

---
+++ src/main/java/com/example/demo/SportsTournamentOrganizerProjectApplication.java
@@ -0,0 +1,30 @@
+package com.example.demo;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import java.util.Arrays;
+
+@SpringBootApplication
+public class SportsTournamentOrganizerProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportsTournamentOrganizerProjectApplication.class, args);
+	}
+
+	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+		return args -> {
+
+			System.out.println("Let's inspect the beans provided by Spring Boot:");
+
+			String[] beanNames = ctx.getBeanDefinitionNames();
+			Arrays.sort(beanNames);
+			for (String beanName : beanNames) {
+				System.out.println(beanName);
+			}
+
+		};
+	}
+}
