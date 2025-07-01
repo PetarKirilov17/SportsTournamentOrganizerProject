cf82b1f69ea03912c91a10630d818489129c52e8
PetarKirilov17 <petar.kirilov17@gmail.com>
2025-07-02 01:27:31 +0300

    restore tournament logic

--- src/main/java/com/example/demo/controllers/TournamentController.java
+++ src/main/java/com/example/demo/controllers/TournamentController.java
@@ -2,8 +2,12 @@ package com.example.demo.controllers;
 
 import com.example.demo.entities.Tournament;
 import com.example.demo.services.TournamentService;
+import com.example.demo.dto.TeamLeaderboardDTO;
+import com.example.demo.repositories.TeamRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/tournaments")
@@ -35,5 +39,10 @@ public class TournamentController {
     @DeleteMapping("/{id}")
     public void deleteTournament(@PathVariable Long id) {
         tournamentService.deleteById(id);
+    }
+
+    @GetMapping("/{id}/leaderboard")
+    public List<TeamLeaderboardDTO> getLeaderboard(@PathVariable Long id) {
+        return tournamentService.getLeaderboard(id);
     }
 } 
--- src/main/java/com/example/demo/services/TournamentService.java
+++ src/main/java/com/example/demo/services/TournamentService.java
@@ -1,18 +1,29 @@
 package com.example.demo.services;
 
+import com.example.demo.dto.TeamLeaderboardDTO;
 import com.example.demo.entities.Tournament;
+import com.example.demo.repositories.TeamRepository;
 import com.example.demo.repositories.TournamentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class TournamentService extends BasicService<Tournament> {
     @Autowired
     private TournamentRepository tournamentRepository;
 
+    @Autowired
+    private TeamRepository teamRepository;
+
     @Override
     protected JpaRepository<Tournament, Long> getRepository() {
         return tournamentRepository;
+    }
+
+    public List<TeamLeaderboardDTO> getLeaderboard(Long id) {
+        return teamRepository.getLeaderboardForTournament(id);
     }
 } 
